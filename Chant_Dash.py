a9d09ced1c6ce22507c74e1cbbdebb2fcab7ce16
carlb59 <163639693+carlb59@users.noreply.github.com>
2024-10-22 19:09:50 +0200

    Update Chant_Dash.py

--- Chant_Dash.py
+++ Chant_Dash.py
@@ -7,6 +7,7 @@ df = pd.read_excel('chant.xlsx')
 df['Date']= pd.DatetimeIndex(df['Date']).strftime('%d-%m-%Y')
 df['Resp']=df['Resp'].astype(str)
 
+app = dash.Dash(_name_)
 app = app.server
 
 app.layout = html.Div(children=[
