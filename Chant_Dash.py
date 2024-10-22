8ca2bfec6da123e979dc74473dc72a58ed3e0049
carlb59 <163639693+carlb59@users.noreply.github.com>
2024-10-22 15:50:45 +0200

    Add files via upload

--- Chant_Dash.py
+++ Chant_Dash.py
@@ -1,70 +1,88 @@
 import pandas as pd
-import numpy as np
-import datetime as dt
-from openpyxl import load_workbook
-from openpyxl.styles import PatternFill, Font, Border, Alignment, NamedStyle
-from openpyxl.chart import LineChart, Reference, Series
-from openpyxl .utils import get_column_letter
-from typing import List
-from enum import Enum
-from datetime import date, timedelta
-import plotly.graph_objects as go
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
-from dash.dash_table import DataTable, FormatTemplate
-from dash.dash_table.Format import Format, Group, Scheme, Symbol, Trim
-import plotly.express as px
+import dash_bootstrap_components as dbc
+from dash import Dash, dcc, html, dash_table, Input, Output, State, callback
+from dash.dash_table.Format import Format, Group, Scheme, Symbol
 
 df = pd.read_excel('chant.xlsx')
 df['Date']= pd.DatetimeIndex(df['Date']).strftime('%d-%m-%Y')
+df['Resp']=df['Resp'].astype(str)
 
-app = Dash(__name__)
-server = app.server
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div(children=[
-    html.H1(children='DOUIN DashBoard Chantier en mouvement'),
-        dash_table.DataTable(
-                         data=df.to_dict('records'),
-                         style_data={'whiteSpace': 'normal', 'height': 'auto', 'lineHeight': '15px'}, 
-                         columns=[
-                            dict(name='Chantier', id='Code chantier', type='text'),
-                            dict(name='Nom du chantier', id='Lib', type='text'),
-                            dict(name= 'Responsable', id='Resp', type='text'),
-                            dict(name='Type', id='Type', type='text'),
-                            dict(name='Date', id= 'Date', type='datetime'),
-                            dict(name='Charges', id='Charge', type='numeric', format=Format(precision=2, scheme=Scheme.fixed, group=Group.yes,
-                groups=3,
-                group_delimiter='.',
-                decimal_delimiter=',',
-                symbol=Symbol.yes,
-                symbol_prefix=u'€')),
-                            dict(name='Facturation', id='Produit', type='numeric', format=Format(precision=2, scheme=Scheme.fixed, group=Group.yes,
-                groups=3,
-                group_delimiter='.',
-                decimal_delimiter=',',
-                symbol=Symbol.yes,
-                symbol_prefix=u'€')),
-                            dict(name='Résultat', id='Resultat', type='numeric', format=Format(precision=2, scheme=Scheme.fixed, group=Group.yes,
-                groups=3,
-                group_delimiter='.',
-                decimal_delimiter=',',
-                symbol=Symbol.yes,
-                symbol_prefix=u'€')),
-                            dict(name='Coeff', id='Coeff', type='numeric', format=Format(precision=2, scheme=Scheme.fixed)),
-                            dict(name='Avancement', id='Avance', type='numeric', format=Format(precision=2, scheme=Scheme.fixed, group=Group.yes,
-                groups=3,
-                group_delimiter='.',
-                decimal_delimiter=',',
-                symbol=Symbol.yes,
-                symbol_prefix=u'€'))
-                            ],
-                         filter_action = 'native',
-                         sort_action = 'native',
-                         page_size=15, 
-                         style_as_list_view=True,
-                         style_cell_conditional=[{'if': {'column_id': 'Lib'}, 'textAlign': 'left'}],
-                         style_cell={'padding': '5px'},
-                         style_header={'backgroundColor': 'rgb(220,220,220)'})
-])
+html.H1(children='DOUIN DashBoard Chantier en mouvement :'),
+dbc.Row([
+    dbc.Col([html.H4(children='Sélection de chantiers : ')]),
+    dbc.Col([lchant := dcc.Dropdown([x for x in sorted(df['Code chantier'].unique())], searchable=True, multi=True)]),
+    dbc.Col([html.H4(children='Sélection de Responsables : ')]),
+    dbc.Col([lresp := dcc.Dropdown([x for x in sorted(df['Resp'].unique())], searchable=True, multi=True)])
+    ]),
+html.H4(children='Table chantier :'),
+tab_chant := dash_table.DataTable(
+    data=df.to_dict('records'),
+    style_data={'whiteSpace': 'normal', 'height': 'auto', 'lineHeight': '15px'}, 
+    columns=[
+    dict(name='Chantier', id='Code chantier', type='text'),
+    dict(name='Nom du chantier', id='Lib', type='text'),
+    dict(name= 'Responsable', id='Resp', type='text'),
+    dict(name='Type', id='Type', type='text'),
+    dict(name='Date', id= 'Date', type='datetime'),
+    dict(name='Charges', id='Charge', type='numeric', format=Format(precision=2, scheme=Scheme.fixed, group=Group.yes,
+        groups=3,
+        group_delimiter='.',
+        decimal_delimiter=',',
+        symbol=Symbol.yes,
+        symbol_prefix=u'€')),
+    dict(name='Facturation', id='Produit', type='numeric', format=Format(precision=2, scheme=Scheme.fixed, group=Group.yes,
+        groups=3,
+        group_delimiter='.',
+        decimal_delimiter=',',
+        symbol=Symbol.yes,
+        symbol_prefix=u'€')),
+    dict(name='Resultat', id='Resultat', type='numeric', format=Format(precision=2, scheme=Scheme.fixed, group=Group.yes,
+        groups=3,
+        group_delimiter='.',
+        symbol=Symbol.yes,
+        symbol_prefix=u'€')),
+    dict(name='Coefficient', id='Coeff', type='numeric', format=Format(precision=3, scheme=Scheme.fixed)),
+    dict(name='Avancement', id='Avance', type='numeric', format=Format(precision=2, scheme=Scheme.fixed, group=Group.yes,
+        groups=3,
+        group_delimiter='.',
+        symbol=Symbol.yes,
+        symbol_prefix=u'€')),
+    ],        
+        filter_action='native',
+        sort_action='native',
+        page_size=50,
+        style_data_conditional=[
+        {
+            'if': {
+                'filter_query': '{Resultat} <0',
+                'column_id': 'Resultat',
+            },
+            'backgroundColor': 'red',
+            'color': 'white'
+        }],
+        style_header={'backgroundColor': 'rgb(220,220,220)'},
+)])
+
+
+@callback(
+    Output(tab_chant, 'data'),
+    Input(lchant, 'value'),
+    Input(lresp, 'value')
+)
+def update_dropdown(lchant_v, lresp_v):
+    dff = df.copy()
+
+    if lchant_v:
+        dff = dff[dff['Code chantier'].isin(lchant_v)]
+
+    if lresp_v:
+        dff = dff[dff['Resp'].isin(lresp_v)]
+
+    return dff.to_dict('records')
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
