81d33d5262844d5efde900e0292d885e00081733
carlb59 <163639693+carlb59@users.noreply.github.com>
2024-10-23 12:16:32 +0200

    Update Chant_Dash.py

--- Chant_Dash.py
+++ Chant_Dash.py
@@ -8,7 +8,7 @@ df['Date']= pd.DatetimeIndex(df['Date']).strftime('%d-%m-%Y')
 df['Resp']=df['Resp'].astype(str)
 
 Chant_Dash = Dash(__name__)
-server = app.server
+server = Chant_Dash.server
 
 app.layout = html.Div(children=[
 html.H1(children='DOUIN DashBoard Chantier en mouvement :'),
